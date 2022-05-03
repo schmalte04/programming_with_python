d89c8e13665e91dcefbc2ec3d9d5f08c52c45667
schmalte04 <43376541+schmalte04@users.noreply.github.com>
2022-05-03 21:22:45 +0200

    load to db

--- ideal_function_evaluation.py
+++ ideal_function_evaluation.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import sqlalchemy as db
 
 train_input = pd.read_csv('data/train.csv', index_col=0)
 test_input = pd.read_csv('data/test.csv', index_col=0)
 ideal_input = pd.read_csv('data/ideal.csv', index_col=0)
 
+# Defining the Engine
+engine = db.create_engine('sqlite:///users.db', echo=True)
+  
+# Create the Metadata Object
+metadata_obj = db.MetaData()
+
+train_input.to_sql('train_data', engine)
+ideal_input.to_sql('ideal_fucntion',engine)
