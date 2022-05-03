43da695db783aa7b667415ad4a41b2ddb0344afb
schmalte04 <43376541+schmalte04@users.noreply.github.com>
2022-05-03 21:59:31 +0200

    plot prep

--- ideal_function_evaluation.py
+++ ideal_function_evaluation.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import sqlalchemy as db
+import numpy as np
 
+
+#### Frist step: Read the CSV files and store them as objects
 train_input = pd.read_csv('data/train.csv', index_col=0)
 test_input = pd.read_csv('data/test.csv', index_col=0)
 ideal_input = pd.read_csv('data/ideal.csv', index_col=0)
 
-# Defining the Engine
-engine = db.create_engine('sqlite:///users.db', echo=True)
+# Next step: Upload the tables to SQL database
+#engine = db.create_engine('sqlite:///users.db', echo=True)
   
-# Create the Metadata Object
-metadata_obj = db.MetaData()
+#metadata_obj = db.MetaData()
+#train_input.to_sql('train_data', engine)
 
-train_input.to_sql('train_data', engine)
-ideal_input.to_sql('ideal_fucntion',engine)
+x=train_input[:,0]
+y=train_input[:,1]
+
+
+print(train_input.iloc[:,0:1])
+
+
+glyph = Scatter(x="x", y="y", size="sizes", marker="square")
+plot.add_glyph(source, glyph)
