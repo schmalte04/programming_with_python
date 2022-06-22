dae80026771d5b069bc1439cade97712290fb6cf
schmalte04 <43376541+schmalte04@users.noreply.github.com>
2022-06-22 17:04:33 +0200

    2206_assignment

--- functions.py
+++ functions.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 import math
+import sqlalchemy as db
+from sqlalchemy import create_engine
 
 def ClassifierFunction(ToClassify, IdealFunction):
+
+    ToClassify['No. of ideal func'] = ""
+    ToClassify['Delta Y (test func)'] = "" 
 
     for i in range(2,5):
     
@@ -10,10 +15,111 @@ def ClassifierFunction(ToClassify, IdealFunction):
 
             test_point_deviation = abs(np.subtract(ToClassify.y[x_point],ToClassify.iloc[x_point,i]))
             
+            
 
             if test_point_deviation.max()<IdealFunction.iloc[2,i-2]:        
                 ToClassify.iloc[x_point,6] = IdealFunction.iloc[0,i-2]
-                ToClassify.iloc[x_point,7] = test_point_deviation.max()
+                ToClassify.iloc[x_point,7] = test_point_deviation.max().round(2)
             
+            ToClassify.round(4)
             
     return(ToClassify)
+
+
+def FindIdealFunction(train_input,ideal_input,df_results_best_function,df_least_deviation):
+
+    train_set = df_results_best_function.columns
+
+    for e in train_set: 
+
+        l = ideal_input.columns
+        
+        for i in l[1:]:
+
+            ### Subtract Training Y - Ideal Function Y and calc deviation and the sum of squared deviation
+            df_least_deviation.Sum_Squared_Deviation[i] = sum((np.subtract(train_input[e],ideal_input[i])) **2)    
+            df_least_deviation.Deviation[i] = (np.subtract(train_input[e],ideal_input[i]))   
+        
+        ### Check which Ideal Function has the lowest Sum of Squared Deviations
+        df_results_best_function.loc['Best_Function',e] = df_least_deviation['Sum_Squared_Deviation'].astype('float64').idxmin()
+       
+        
+
+        df_deviations = pd.DataFrame(
+            {'train': train_input[e], 
+            'ideal_function': ideal_input[df_least_deviation['Sum_Squared_Deviation'].astype('float64').idxmin()], 
+            'deviation': np.subtract(train_input[e],ideal_input[df_least_deviation['Sum_Squared_Deviation'].astype('float64').idxmin()])})
+       
+        df_results_best_function.loc['MaxDeviation',e] = df_deviations['deviation'].max()
+        df_results_best_function.loc['ClassificationThreshold',e] = df_deviations['deviation'].max()*math.sqrt(2)
+
+    return(df_results_best_function)
+
+
+
+def CreateIdealFunctionHeader(df):
+    
+    IdealFunctionList = ['x']
+    
+    for e in df:
+        IdealFunctionList.append(df.loc['Best_Function',e])
+    
+    return(IdealFunctionList)
+
+
+
+class utility:
+
+    def __init__(self, csvPath):
+        # csvpath param represents the input-data/filename
+        # Here we are parsing the csv file into list of dataList and later iterate the object to get details
+        # We need specific structure for csv in which 1st column is x and following column is y values
+        self.dataFrames = []
+
+        # The csv is being read by the Panda module and turned into a dataframe
+        try:
+            self.csv = pd.read_csv(csvPath)
+        except FileNotFoundError:
+            print("Could not read file {}".format(csvPath))
+            raise
+
+
+def toSql(obj, fileName, suffix):
+        ###
+        # This function accepts the filename and that is the name the db gets
+        # the same function has suffix to specify based on original col name
+        # Also, if the db file already exist in that case it will be replaced with new one
+        # Here we are using sqlalchemy to handle all db related operations
+        ###
+
+        engineDB = db.create_engine('sqlite:///{}.db'.format(fileName), echo=False)
+
+        # save file to db
+        csvDataCopied = obj.copy()
+        csvDataCopied.columns = [name.capitalize() + suffix for name in csvDataCopied.columns]
+        csvDataCopied.set_index(csvDataCopied.columns[0], inplace=True)
+
+        csvDataCopied.to_sql(
+            fileName,
+            engineDB,
+            if_exists="replace",
+            index=True,
+        )
+
+
+
+def createGraphFromTwoFunctions(scatterFunction, lineFunction):
+    
+    # first function dataframes and names
+    functionOneDataframe = scatterFunction.dataframe
+    functionOneName = scatterFunction.name
+
+    # Second function dataframes and names
+    functionTwoDataframe = lineFunction.dataframe
+    functionTwoName = lineFunction.name
+
+    graphPlot = figure(title="Graph for train model {} vs ideal {}.".format(functionOneName, functionTwoName),
+               x_axis_label='x', y_axis_label='y')
+    graphPlot.scatter(functionOneDataframe["x"], functionOneDataframe["y"], fill_color="green", legend_label="Train")
+    graphPlot.line(functionTwoDataframe["x"], functionTwoDataframe["y"], legend_label="Ideal", line_width=5)
+    return graphPlot
--- ideal_function_evaluation.py
+++ ideal_function_evaluation.py
@@ -1,59 +1,86 @@
 import pandas as pd
-import sqlalchemy as db
+from sqlalchemy import create_engine,  column
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import unittest
 from functions import ClassifierFunction
+from functions import FindIdealFunction
+from functions import CreateIdealFunctionHeader
+from functions import utility
+from functions import toSql
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import row, column
 
+if __name__ == '__main__':
 
 #### Frist step: Read the CSV files and store them as objects
-train_input = pd.read_csv('data/train.csv')
-test_input = pd.read_csv('data/test.csv')
-ideal_input = pd.read_csv('data/ideal.csv')
+    path_train = 'data/train.csv'
+    path_test = 'data/test.csv'
+    path_ideal = 'data/ideal.csv'
 
-# Next step: Upload the tables to SQL database
-#engine = db.create_engine('sqlite:///users.db', echo=True)
-  
-#metadata_obj = db.MetaData()
-#train_input.to_sql('train_data', engine)
+    ideal_input = utility(csvPath=path_ideal).csv
+    train_input = utility(csvPath=path_train).csv
+    test_input = utility(csvPath=path_test).csv
 
+    # convert aideal and training files to sql using sqlalchemy
+    toSql(obj=ideal_input,fileName="ideal", suffix=" (ideal function)")
+    toSql(obj=train_input,fileName="training", suffix=" (training function)")
+
+
+### Prepare Result dataframes to be passed to the function 
 df_results_best_function= pd.DataFrame({'y1': ["NaN","NaN","NaN"],'y2': ["NaN","NaN","NaN"],'y3': ["NaN","NaN","NaN"],'y4': ["NaN","NaN","NaN"]}, index=['Best_Function','MaxDeviation',"ClassificationThreshold"])
-
 df_least_deviation = pd.DataFrame(index=ideal_input.columns[1:], columns=["Sum_Squared_Deviation","Deviation"])
 
-train_set = df_results_best_function.columns
-ideal_function_columns = ['x']
+### Calculate Ideal Functions
+df_results_best_function = FindIdealFunction(train_input,ideal_input,df_results_best_function,df_least_deviation)
 
-for e in train_set: 
-
-        l = ideal_input.columns
+output_file("training.html")
         
-        for i in l[1:]:
+# instantiating the figure object
+graph1 = figure(title = "Scatter Plot Training Data",x_axis_label='x', y_axis_label='y')
+graph1.scatter(train_input.loc[:,'x'], train_input.loc[:,'y1'], color='#000000', size=10, legend_label='Training Data Y1')
+graph1.scatter(ideal_input.loc[:,'x'], ideal_input.loc[:,'y21'], color='#0000ff', size=10, legend_label='Ideal Function')
 
-            df_least_deviation.Sum_Squared_Deviation[i] = sum((np.subtract(train_input[e],ideal_input[i])) **2)    
-            df_least_deviation.Deviation[i] = (np.subtract(train_input[e],ideal_input[i]))   
-        
-        df_results_best_function.loc['Best_Function',e] = df_least_deviation['Sum_Squared_Deviation'].astype('float64').idxmin()
-       
-        ideal_function_columns.append(df_results_best_function.loc['Best_Function',e])
+graph2 = figure(title = "Scatter Plot Training Data",x_axis_label='x', y_axis_label='y')
+graph2.scatter(train_input.loc[:,'x'], train_input.loc[:,'y2'], color='#000000', size=10, legend_label='Training Data Y2')
+graph2.scatter(ideal_input.loc[:,'x'], ideal_input.loc[:,'y10'], color='#0000ff', size=10, legend_label='Ideal Function')
 
-        df_deviations = pd.DataFrame({'train': train_input[e], 'ideal_function': ideal_input[df_least_deviation['Sum_Squared_Deviation'].astype('float64').idxmin()], 'deviation': np.subtract(train_input[e],ideal_input[df_least_deviation['Sum_Squared_Deviation'].astype('float64').idxmin()])})
-       
-        df_results_best_function.loc['MaxDeviation',e] = df_deviations['deviation'].max()
-        df_results_best_function.loc['ClassificationThreshold',e] = df_deviations['deviation'].max()*math.sqrt(2)
+graph3 = figure(title = "Scatter Plot Training Data",x_axis_label='x', y_axis_label='y')
+graph3.scatter(train_input.loc[:,'x'], train_input.loc[:,'y3'], color='#000000', size=10, legend_label='Training Data Y3')
+graph3.scatter(ideal_input.loc[:,'x'], ideal_input.loc[:,'y18'], color='#0000ff', size=10, legend_label='Ideal Function')
 
-           
+graph4 = figure(title = "Scatter Plot Training Data",x_axis_label='x', y_axis_label='y')
+graph4.scatter(train_input.loc[:,'x'], train_input.loc[:,'y4'], color='#000000', size=10, legend_label='Training Data Y4')
+graph4.scatter(ideal_input.loc[:,'x'], ideal_input.loc[:,'y15'], color='#0000ff', size=10, legend_label='Ideal Function')
+
+show(column(graph1, graph2,graph3,graph4))
+
+### Create a Object with x and the names of the four ideal functions to extract them in the next step
+ideal_function_columns = CreateIdealFunctionHeader(df_results_best_function)
+
+
 SetIdealFunctionData = ideal_input.loc[:,ideal_function_columns]
-
 test_input = test_input.merge(SetIdealFunctionData, on='x', how='left')
 
-test_input['Classifier'] = "NaN"
-test_input['Deviation'] = "NaN" 
 
+### Classifier Function:
+'''
+1) Calulates Deviation between Y_Test and Y_Ideal_Functions at point x
+2) Checks weather deviation is smaller than the largest deviation of the ideal_functions with the training set by factor sqrt(2) "Critical Threshold
+3) If a deviation between Y_Test and Y_Ideal_Function is within the threshold, it will be mapped
 
-### loop over ideal functions and check if classifier of test set is within threshold
+Result is a table with mapped Ideal Functions if possible for each Test Point given and the deviations.
 
-
+Required Inputs: Test_Input and df_results_best_function (result from the first part of the assignment)
+'''
 Classifier_Results = ClassifierFunction(test_input,df_results_best_function)
 print(Classifier_Results.to_markdown())
+
+
+toSql(obj=Classifier_Results.loc[:,['x','y','Delta Y (test func)','No. of ideal func']],fileName="ClassifiedTest", suffix=" (test classification)")
+
+
+#### last step: vizualize the data properly and save it
+
+
